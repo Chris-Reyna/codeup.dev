32839646f3707b5c9e138a2ce625e673e3edae02
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-25 14:06:22 -0600

    Update correction
    move here link to upload file to body

--- public/todo-list.php
+++ public/todo-list.php
@@ -9,56 +9,53 @@ echo "<p>FILE</p>";
 var_dump($_FILES);
 
 
-                $filename = "to_do_list.txt";
-                //load file return an array
-                function openFile($filename) {
-                    $handle = fopen($filename, "r");
-                    $content = fread($handle, filesize($filename));
-                    fclose($handle);
-                    return explode("\n", $content);
-                }
-                //save an array to a flat file
-                function saveFile($filename, $items) {
-                    $itemStr = implode("\n", $items);
-                    $handle = fopen($filename, "w");
-                    fwrite($handle, $itemStr);
-                    fclose($handle);
-                }
+$filename = "to_do_list.txt";
+//load file return an array
+function openFile($filename) {
+    $handle = fopen($filename, "r");
+    $content = fread($handle, filesize($filename));
+    fclose($handle);
+    return explode("\n", $content);
+}
+//save an array to a flat file
+function saveFile($filename, $items) {
+    $itemStr = implode("\n", $items);
+    $handle = fopen($filename, "w");
+    fwrite($handle, $itemStr);
+    fclose($handle);
+}
 
-                $items = openFile($filename);
+$items = openFile($filename);
 
-                //Load file
-                if (isset($_POST['TASK']) && !empty($_POST['TASK'])) {
-                    $item = $_POST['TASK'];
-                    array_push($items, $item);
-                    saveFile($filename, $items);
-                }
-                //remove
-                if (isset($_GET['remove'])) {
-                    $item2 = $_GET['remove'];
-                    unset($items[$item2]);
-                    saveFile($filename, $items);
+//Load file
+if (isset($_POST['TASK']) && !empty($_POST['TASK'])) {
+    $item = $_POST['TASK'];
+    array_push($items, $item);
+    saveFile($filename, $items);
+}
+//remove
+if (isset($_GET['remove'])) {
+    $item2 = $_GET['remove'];
+    unset($items[$item2]);
+    saveFile($filename, $items);
 
-                    header("Location: todo-list.php");
-                    exit;
-                }
-                //Verify there were uploaded files and no errors
-                if (count($_FILES) > 0 && $_FILES['upload_file']['error'] == 0) {
-                    // Set the destination directory for uploads
-                    $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
-                    // Grab the filename from the uploaded file by using basename
-                    $pathname = basename($_FILES['upload_file']['name']);
-                    // Create the saved filename using the file's original name and our upload directory
-                    $saved_filename = $upload_dir . $pathname;
-                    // Move the file from the temp location to our uploads directory
-                    move_uploaded_file($_FILES['upload_file']['tmp_name'], $saved_filename);
-                }
+    header("Location: todo-list.php");
+    exit;
+}
+//Verify there were uploaded files and no errors
+if (count($_FILES) > 0 && $_FILES['upload_file']['error'] == 0) {
+    // Set the destination directory for uploads
+    $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+    // Grab the filename from the uploaded file by using basename
+    $pathname = basename($_FILES['upload_file']['name']);
+    // Create the saved filename using the file's original name and our upload directory
+    $saved_filename = $upload_dir . $pathname;
+    // Move the file from the temp location to our uploads directory
+    move_uploaded_file($_FILES['upload_file']['tmp_name'], $saved_filename);
+}
 
-                //Check if we saved a file
-                if (isset($saved_filename)) {
-                    // If we did, show a link to the uploaded file
-                    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
-                }
+//Check if we saved a file
+
 ?>
 <!DOCTYPE>
 <html>
@@ -70,13 +67,15 @@ var_dump($_FILES);
 		<h2>LIST of tasks</h2>
 			<ul>
 				
-				<?foreach ($items as $key => $item) : ?>
-						<li><?= $item?> | <a href= "?remove=<?= $key?>">Mark as complete</a> </li>
-				<? endforeach ?>
-                
+			 <?foreach ($items as $key => $item) : ?>
+				<li><?= $item?> | <a href= "?remove=<?= $key?>">Mark as complete</a> </li>
+			 <? endforeach ?>
+             <?if (isset($saved_filename)) : ?>
+                <?= "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>"?>
+             <?endif?> 
 			</ul>
 
-			<h3>Add Tasks to list</h3>
+		<h3>Add Tasks to list</h3>
 		<form method="POST" enctype="multipart/form-data" action="todo-list.php">
 	    	<p>
 	        	<label for="TASK">TASK</label>
@@ -86,8 +85,8 @@ var_dump($_FILES);
 	        	<button type="submit">ADD</button>
 	    	</p>	
 		</form>
-        <h1>Upload File</h1>
 
+        <h3>Upload File</h3>
         <form method="POST" enctype="multipart/form-data" action="todo-list.php">
             <p>
               <label for="upload_file">File to add to list</label>
