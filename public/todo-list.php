7460d13e75b1ff46dbd32fc47e1ea6e904a22c79
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-25 13:46:36 -0600

    update align and move php
    tab changes, moved php out of html and used alternative syntax

--- public/todo-list.php
+++ public/todo-list.php
@@ -8,53 +8,40 @@ var_dump($_POST);
 echo "<p>FILE</p>";
 var_dump($_FILES);
 
-?>
-<!DOCTYPE>
-<html>
-	<head>
-		<title>TODO List</title>
-	</head>
-	<body>
-		<h1>TODO List</h1>
-		<h2>LIST of tasks</h2>
-			<ul>
-				<?php
-				$filename = "to_do_list.txt";
-				//load file return an array
-    			function openFile($filename) {
-    				$handle = fopen($filename, "r");
-    				$content = fread($handle, filesize($filename));
-    				fclose($handle);
-    				return explode("\n", $content);
-    			}
-    			//save an array to a flat file
-    			function saveFile($filename, $items) {
-    				$itemStr = implode("\n", $items);
-    				$handle = fopen($filename, "w");
-    				fwrite($handle, $itemStr);
-    				fclose($handle);
-    			}
 
-    			$items = openFile($filename);
+                $filename = "to_do_list.txt";
+                //load file return an array
+                function openFile($filename) {
+                    $handle = fopen($filename, "r");
+                    $content = fread($handle, filesize($filename));
+                    fclose($handle);
+                    return explode("\n", $content);
+                }
+                //save an array to a flat file
+                function saveFile($filename, $items) {
+                    $itemStr = implode("\n", $items);
+                    $handle = fopen($filename, "w");
+                    fwrite($handle, $itemStr);
+                    fclose($handle);
+                }
 
-    			//Load file
-    			if (isset($_POST['TASK']) && !empty($_POST['TASK'])) {
-    				$item = $_POST['TASK'];
-    				array_push($items, $item);
-    				saveFile($filename, $items);
-    			}
-    			//remove
-    			if (isset($_GET['remove'])) {
-    				$item2 = $_GET['remove'];
-    				unset($items[$item2]);
-    				saveFile($filename, $items);
+                $items = openFile($filename);
 
-    				header("Location: todo-list.php");
-    				exit;
-    			}
-				foreach ($items as $key => $item) {
-						echo "<li>$item | <a href=\"?remove=$key\">Mark as complete</a> </li>";
-				};
+                //Load file
+                if (isset($_POST['TASK']) && !empty($_POST['TASK'])) {
+                    $item = $_POST['TASK'];
+                    array_push($items, $item);
+                    saveFile($filename, $items);
+                }
+                //remove
+                if (isset($_GET['remove'])) {
+                    $item2 = $_GET['remove'];
+                    unset($items[$item2]);
+                    saveFile($filename, $items);
+
+                    header("Location: todo-list.php");
+                    exit;
+                }
                 //Verify there were uploaded files and no errors
                 if (count($_FILES) > 0 && $_FILES['upload_file']['error'] == 0) {
                     // Set the destination directory for uploads
@@ -72,9 +59,21 @@ var_dump($_FILES);
                     // If we did, show a link to the uploaded file
                     echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
                 }
-
+?>
+<!DOCTYPE>
+<html>
+	<head>
+		<title>TODO List</title>
+	</head>
+	<body>
+		<h1>TODO List</h1>
+		<h2>LIST of tasks</h2>
+			<ul>
 				
-				?>
+				<?foreach ($items as $key => $item) : ?>
+						<li><?= $item?> | <a href= "?remove=<?= $key?>">Mark as complete</a> </li>
+				<? endforeach ?>
+                
 			</ul>
 
 			<h3>Add Tasks to list</h3>
