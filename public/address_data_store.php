8df8256a03ac39fab00fe6374ca2f2eec3ede2d9
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-28 14:34:27 -0600

    removed
    refactored methods to call from parent methods, deleted contract and
    public property and added them to parent class
    functions and added them to parent class

--- public/address_data_store.php
+++ public/address_data_store.php
@@ -4,35 +4,12 @@ require_once('filestore.php');
 
 class AddressDataStore extends Filestore {
 
-   // public $filename = '';
-
-    //function __construct($file = ''){
-    // 	$this->filename = $file;
-
-    // }
-
     function read_address_book(){
         return $this-> read_csv();
     }
-
-    // $content=[];
-    // $handle = fopen($this->filename, "r");
-    // while(($data = fgetcsv($handle)) !== FALSE){
-    // 	$content[] = $data;
-    // }
-    // fclose($handle);
-    // return $content;
     
-
     function write_address_book($array) {
         return $this-> write_csv($array);
     }
-  //     $handle = fopen($this->filename, 'w');
-		// foreach ($addresses_array as $array) {
-		// 	fputcsv($handle, $array);
-		// }
-		// fclose($handle);
-  //   }
-
 }
 ?>
