d7d9bd71c0f5340da5f1d5534ec741e1c87bc361
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-28 15:52:10 -0600

    new construct
    overwriting construct calling parent construct

--- public/address_data_store.php
+++ public/address_data_store.php
@@ -4,12 +4,17 @@ require_once('filestore.php');
 
 class AddressDataStore extends Filestore {
 
+   function __construct($filename=''){
+        $filename= strtolower($filename);
+        parent::__construct($filename);
+    }
     function read_address_book(){
         return $this-> read_csv();
     }
-    
+
     function write_address_book($array) {
         return $this-> write_csv($array);
     }
-}
+
+} 
 ?>
