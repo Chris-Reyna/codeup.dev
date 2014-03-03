e8a0e9a377fca434c8670f498889a6a24c8147a6
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-03 11:17:23 -0600

    added pub method and change old to private
    now have two pub methods that contain each write and each read. changed
    fur original methods to private

--- public/filestore.php
+++ public/filestore.php
@@ -1,26 +1,50 @@
 <?php
 class Filestore{
 	public $filename ='';
+	private $is_csv = FALSE;
 
 	public function __construct($filename= ''){
 		$this->filename = $filename;
+		if(substr($filename, -3) == 'csv'){
+		$this->is_csv == TRUE;
+		}
 	}
+
+	public function read() {
+		if($this->is_csv == TRUE){
+			return $this->read_csv();
+		}else{
+			return $this->read_lines();
+		}
+	}
+
+	public function write($array){
+		if($this->is_csv == TRUE){
+			$this->write_csv($array);
+		}else{
+			$this->write_lines($array);
+		}
+	}
+
 	//return array of lines in $this->filename
-	public function read_lines(){
+	private function read_lines(){
+		if (filesize($this->filename) == 0){
+			return [];
+		}
 		$handle = fopen($this->filename, "r");
     	$content = fread($handle, filesize($this->filename));
     	fclose($handle);
     	return explode("\n", $content);
 	}
 	//write each element in $array to a new line in this->filename
-	public function write_lines($array){
+	private function write_lines($array){
 		$itemStr = implode("\n", $array);
     	$handle = fopen($this->filename, "w");
     	fwrite($handle, $itemStr);
     	fclose($handle);
 	}
 	//Reads content of csv $this->filename, returns an array
-	public function read_csv(){
+	private function read_csv(){
 		$content=[];
     	$handle = fopen($this->filename, "r");
     	while(($data = fgetcsv($handle)) !== FALSE){
@@ -30,7 +54,7 @@ class Filestore{
     	return $content;
 	}
 	//Writes contents of $array to csv $this->filename
-	public function write_csv($array){
+	private function write_csv($array){
 		$handle = fopen($this->filename, 'w');
 		foreach ($array as $item) {
 			fputcsv($handle, $item);
