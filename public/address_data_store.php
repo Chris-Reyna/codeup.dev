4d452766c2d142480ec523857057c51dcffa6f11
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-03 10:04:51 -0600

    added public visibility to methods
    specified that methods are public

--- public/address_data_store.php
+++ public/address_data_store.php
@@ -4,15 +4,15 @@ require_once('filestore.php');
 
 class AddressDataStore extends Filestore {
 
-   function __construct($filename=''){
+   public function __construct($filename=''){
         $filename= strtolower($filename);
         parent::__construct($filename);
     }
-    function read_address_book(){
+    public function read_address_book(){
         return $this-> read_csv();
     }
 
-    function write_address_book($array) {
+    public function write_address_book($array) {
         return $this-> write_csv($array);
     }
 
--- public/filestore.php
+++ public/filestore.php
@@ -2,25 +2,25 @@
 class Filestore{
 	public $filename ='';
 
-	function __construct($filename= ''){
+	public function __construct($filename= ''){
 		$this->filename = $filename;
 	}
 	//return array of lines in $this->filename
-	function read_lines(){
+	public function read_lines(){
 		$handle = fopen($this->filename, "r");
     	$content = fread($handle, filesize($this->filename));
     	fclose($handle);
     	return explode("\n", $content);
 	}
 	//write each element in $array to a new line in this->filename
-	function write_lines($array){
+	public function write_lines($array){
 		$itemStr = implode("\n", $array);
     	$handle = fopen($this->filename, "w");
     	fwrite($handle, $itemStr);
     	fclose($handle);
 	}
 	//Reads content of csv $this->filename, returns an array
-	function read_csv(){
+	public function read_csv(){
 		$content=[];
     	$handle = fopen($this->filename, "r");
     	while(($data = fgetcsv($handle)) !== FALSE){
@@ -30,7 +30,7 @@ class Filestore{
     	return $content;
 	}
 	//Writes contents of $array to csv $this->filename
-	function write_csv($array){
+	public function write_csv($array){
 		$handle = fopen($this->filename, 'w');
 		foreach ($array as $item) {
 			fputcsv($handle, $item);
