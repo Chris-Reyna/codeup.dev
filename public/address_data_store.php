06a2be3092ad3dd838ed8682dfea0959284be81e
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-03 14:24:30 -0600

    added method char_limit
    created function to set char limit at 125

--- public/address_data_store.php
+++ public/address_data_store.php
@@ -15,6 +15,13 @@ class AddressDataStore extends Filestore {
     public function write_address_book($array) {
         return $this->write($array);
     }
-
+    //set character limit to 125 for input
+    public function char_limit($array) {
+        foreach($array as $key=>$value){
+            if(strlen($value) > 125){
+                throw new Exception("Content exceeds 125 characters");
+            }
+        }
+    }
 } 
 ?>
