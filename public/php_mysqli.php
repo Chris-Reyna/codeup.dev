b0263dc36dcad6cf00050a5b80c9a89ff3c0b617
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-13 15:37:16 -0500

    deleted create table; added input
    array with first entry, and for each

--- public/php_mysqli.php
+++ public/php_mysqli.php
@@ -7,21 +7,19 @@ $mysqli = new mysqli('127.0.0.1', 'creyna', 'chris', 'codeup_mysqli_test_db');
 if ($mysqli->connect_errno) {
     echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
 }
-// Create the query and assign to var
-$query = 'CREATE TABLE national_parks (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(75) NOT NULL,
-    location VARCHAR(20) NOT NULL,
-    description VARCHAR(500) NOT NULL,
-    date_established VARCHAR(20) NOT NULL,
-    area_in_acres VARCHAR(20) NOT NULL,
-    PRIMARY KEY (id)
-);';
 
-if (!$mysqli->query($query)) {
-    throw new Exception("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);
-}
+$parks = [
+	['name'=>'Acadia', 'location'=>'Maine', 'description'=>'Covering most of Mount Desert Island and other coastal islands, Acadia features the tallest mountain on the Atlantic coast of the United States, granite peaks, ocean shoreline, woodlands, and lakes. There are freshwater, estuary, forest, and intertidal habitats.', 'date_established'=> '1919-02-26', 'area_in_acres'=> '47,389.67']
+	
+];
+
+foreach ($parks as $park)
+$query = "INSERT INTO national_parks(name, location, description, date_established, area_in_acres) VALUES ('{$park['name']}', '{$park['location']}', '{$park['description']}',
+	'{$park['date_established']}', '{$park['area_in_acres']}');";
+    $mysqli->query($query);
 
 echo $mysqli->host_info . "\n";
+
+
 
 ?>
