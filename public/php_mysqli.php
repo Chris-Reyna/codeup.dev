7af9a1ca8402d6db435d7d8a2e9cbe23e1f73ff8
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-13 15:10:32 -0500

    update create nation park table
    added query to code, is statement calling variable assigned to cease
    table query

--- public/php_mysqli.php
+++ public/php_mysqli.php
@@ -7,6 +7,20 @@ $mysqli = new mysqli('127.0.0.1', 'creyna', 'chris', 'codeup_mysqli_test_db');
 if ($mysqli->connect_errno) {
     echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
 }
+// Create the query and assign to var
+$query = 'CREATE TABLE national_parks (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(75) NOT NULL,
+    location VARCHAR(20) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    date_established VARCHAR(20) NOT NULL,
+    area_in_acres VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);';
+
+if (!$mysqli->query($query)) {
+    throw new Exception("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);
+}
 
 echo $mysqli->host_info . "\n";
 
