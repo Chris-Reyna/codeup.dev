0c7ba95e2bb9dc26857fcdd0c48ae8bc629ae275
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-13 14:20:10 -0500

    initial commit
    code to connect with databSE USING login credentials local host and
    database name

---
+++ public/php_mysqli.php
@@ -0,0 +1,13 @@
+<?php
+
+// Get new instance of MySQLi object
+$mysqli = new mysqli('127.0.0.1', 'creyna', 'chris', 'codeup_mysqli_test_db');
+
+// Check for errors
+if ($mysqli->connect_errno) {
+    echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n";
+
+?>
