a59b80a3dd1d16fb2b1dc7eaebe33bce4a440b6f
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-17 15:04:46 -0500

    update add item
    prepared statement added, $_POST TASK as input from user, able to add
    items to table

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,42 +1,23 @@
 <?php
-require_once('filestore.php');
-require_once('todo_mysql.php')
-$list = new Filestore('to_do_list.txt');
 
-$items = $list->read();
+require_once('todo_mysql.php');
 
-//Load file
-if (isset($_POST['TASK']) && !empty($_POST['TASK'])) {
-    $item = $_POST['TASK'];
-    array_push($items, $item);
-    $list->write($items);
-}
-//remove
-if (isset($_GET['remove'])) {
-    $item2 = $_GET['remove'];
-    unset($items[$item2]);
-    $list->write($items);
 
-    header("Location: todo-list.php");
-    exit;
+
+if(!empty($_POST['TASK'])){
+// // Create the prepared statement
+$stmt = $mysqli->prepare("INSERT INTO Lists (item) VALUES (?)");
+
+// bind parameters
+$stmt->bind_param("s", $_POST['TASK']);
+
+// execute query, return result
+$stmt->execute();
 }
-//Verify there were uploaded files and no errors
-if (count($_FILES) > 0 && $_FILES['upload_file']['error'] == 0) {
-    // Set the destination directory for uploads
-    $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
-    // Grab the filename from the uploaded file by using basename
-    $pathname = basename($_FILES['upload_file']['name']);
-    // Create the saved filename using the file's original name and our upload directory
-    $saved_filename = $upload_dir . $pathname;
-    // Move the file from the temp location to our uploads directory
-    move_uploaded_file($_FILES['upload_file']['tmp_name'], $saved_filename);
-   
-    $newlist= new Filestore($saved_filename);
-    $newitems = $newlist->read($newlist);
-    $items = array_merge($items, $newitems);
-    //var_dump($newitems);
-    $list->write($items);
-}
+
+//query to get the parks
+$result = $mysqli->query("SELECT * FROM Lists");
+
 ?>
 <!DOCTYPE>
 <html>
@@ -47,15 +28,14 @@ if (count($_FILES) > 0 && $_FILES['upload_file']['error'] == 0) {
 	<body>
 		<h1 id="main" class="header">TODO List</h1>
 		<h2 class="header">LIST of tasks</h2>
-			<ul>
-				
-			 <?foreach ($items as $key => $item) : ?>
-				<li><?= htmlspecialchars(strip_tags($item))?> | <a href= "?remove=<?= $key?>">Mark as complete</a> </li>
-			 <? endforeach ?>
-             <?if (isset($saved_filename)) : ?>
-                <?= "<p>You can download your file <a href='/uploads/{$saved_filename}'>here</a>.</p>"?>
-             <?endif?> 
-			</ul>
+			
+				<?php
+                while ($row = $result->fetch_array()) {
+                    echo "<ul>";
+                    echo "<li>" . $row['item'] . "</li>";
+                    echo "</ul>";
+                }
+                ?>
 
 		<h3 class="header">Add Tasks to list</h3>
 		<form method="POST" enctype="multipart/form-data" action="todo-list.php">
